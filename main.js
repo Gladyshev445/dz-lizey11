8684b1aff508bf6b6853bb4bb11d39251d03ecd2
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-03-22 15:06:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+// let x = prompt("Введите первое число")
+//     y = prompt("Введите второе число")
+// if (x == y){
+//     alert("Числа равняются 0")
+//     console.log(x - x)
+//     console.log(y - y)
+// }
+// else{
+//     alert(x + y)
+// }
+
+// let x = prompt("Введите первое число")
+//     y = prompt("Введите второе число")
+// if (x == y){
+//     alert("Числа равняются 0")
+//     console.log(x - x)
+//     console.log(y - y)
+// }
+// else{
+//     x++
+//     y++
+//     alert(x + " и " + y)
+// }
+
+// let x = prompt("Первое значение")
+//     y = prompt("Второе значение")
+// if (x > 0){
+//     x--
+//     alert(x)
+// }
+// if (y > 0){
+//     y++
+//     alert(y)
+// }
+
+// let x = prompt("Первое")
+//     y = prompt("Второе")
+// if (x < y){
+//     alert(x + " потом " + y)
+// }
+// else if (y < x){
+//     alert(y + " потом " + x)
+// }
+
+// let x = prompt("Первое")
+//     y = prompt("Второе")
+// if (x < y){
+//     alert(x)
+// }
+// else if (y < x){
+//     alert(y)
+// }
+
+// let x = prompt("Первое")
+//     y = prompt("Второе")
+// if (x < y){
+//     alert(y)
+// }
+// else if (y < x){
+//     alert(x)
+// }
+
+// let x = prompt("Первое")
+//     y = prompt("Второе")
+//     z = prompt("Третье")
+// if (x < 0 || y > 0 || z > 0){
+//     alert("Отрицательных 1, положительных 2")
+// }
+// else if (x < 0 || y < 0 || z > 0){
+//     alert("Отрицательных 2, положительных 1")
+// }
+// if (x < 0 || y < 0 || z < 0){
+//     alert("Все отрицательные")
+// }
+// else if (x > 0 || y > 0 || z > 0){
+//     alert("Все положительные")
+// }
